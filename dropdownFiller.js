c3ff0aec41e275a1276a0fe444e8d4c6fa24f8fc
Javier Javier <javierjdaza@gmail.com>
2024-09-01 23:33:24 -0500

    add middleware

--- dropdownFiller.js
+++ dropdownFiller.js
@@ -1,4 +1,4 @@
-const phoneOptions = ["iPhone 11.svg", "iPhone 12.svg", "iPhone 13.svg", "iPhone 14.svg", "iPhone 12 Pro.svg", "iPhone 13 Pro.svg", "iPhone 14 Pro.svg", "iPhone 15 Pro.svg", "iPhone 15 Plus.svg", "iPhone 12 Pro Max.svg", "iPhone 14 Pro Max.svg", "iPhone 15 Pro Max.svg", "Samsung S22 Ultra.svg", "Samsung S23 Ultra.svg"];
+const phoneOptions = ["iPhone 11.svg", "iPhone 12.svg", "iPhone 13.svg", "iPhone 14.svg", "iPhone 12 Pro.svg", "iPhone 13 Pro.svg", "iPhone 14 Pro.svg", "iPhone 15 Pro.svg", "iPhone 15 Plus.svg", "iPhone 12 Pro Max.svg", "iPhone 13 Pro Max.svg", "iPhone 14 Pro Max.svg", "iPhone 15 Pro Max.svg", "Samsung S22 Ultra.svg", "Samsung S23 Ultra.svg"];
 
 export function fillDropdown($dropDown) {
 	for (const file of phoneOptions) {
